80e384b4fdaca7fabdb6d54cb06e362a3e3603a6
JonesGP <jonesgamespro1@gmail.com>
2023-08-01 02:30:34 -0300

    v0.0.1

--- RAQIS-UI/app.py
+++ RAQIS-UI/app.py
@@ -1,46 +1,77 @@
 #:kivy 2.2.1
 import kivy
 import sys
-import scripts
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.floatlayout import FloatLayout
-from kivy.graphics import Color, Rectangle
-from kivy.uix.textinput import TextInput
 from kivy.lang import Builder
 from kivy.core.window import Window
 from pathlib import Path
-from kivy.properties import StringProperty
 Builder.load_file('ui.kv')
-
 Window.size = (1280, 720)
-
+Window.minimum_width = 1280
+Window.minimum_height = 720
 #base rgb para colocar no paletton 201010
-class layout_principal(BoxLayout):
+#base rgb para colocar no paletton 6B2F2F claras
+folder = 'a'
+class layoutprincipal(BoxLayout):
     pass
 
 class Meu_app(App): # O App é uma classe do Kivy, nessa linha estamos criando uma classe que herda da classe App do kivy
-    folder_name = StringProperty("ava")
-    def verificar_arq(self, text):
-        aplicativo = App.get_running_app()
+    def def_variable(self):
+        pass
         
+    def verificar_arq(self, text):
+        ap = App.get_running_app()
         folder = Path(text)
         folder_name = folder.name
-        aplicativo.root.ids.pasta.text = f'A pasta é: {folder_name}'
+        ap.root.ids.pasta.text = f'A pasta é: {folder_name}'
         tupla_arq = []
         for arquivo in folder.iterdir():
             tupla_arq.append(arquivo.name)
         formated_tupla_arq = "\n".join(tupla_arq)
-        aplicativo.root.ids.conteudo_pasta.text = str(formated_tupla_arq)
-        print(str(tupla_arq))
-    def menu_two(self):
-        aplicativo = App.get_running_app()
-        aplicativo.root.ids.testebtn.opacity = 1
+        ap.root.ids.conteudo_pasta.text = str(formated_tupla_arq)
+    
+    def tirar_first_char(self, char, dire):
+        folder = Path(dire)
+        for arquivo in folder.iterdir():
+            novo_nome = arquivo.name[int(char):]
+            novo_caminho = arquivo.with_name(novo_nome)
+            arquivo.rename(novo_caminho)
+        pass
+    def remove_last_char(self, dire, char_end):
+        folder = Path(dire)
+        for arquivo in folder.iterdir():
+            old_name, extension = arquivo.stem, arquivo.suffix
+            novo_nome = old_name[:(len(old_name) - int(char_end))] + extension
+            novo_caminho = arquivo.with_name(novo_nome)
+            try:
+                arquivo.rename(novo_caminho)
+            except Exception as error:
+                print(f"Não foi possivel remover os caracteres de: {arquivo.name}")
+                print(f"Ocorreu um erro: {str(error)}")
+    def numerar_arquivos(self, dire):
+        folder = Path(dire)
+        num = 1
+        for arquivo in folder.iterdir():
+            novo_nome = str(num).zfill(2) + " - " + arquivo.name #.zfill(2) serve para colocar 2 digitos zeros se o arquivo tiver menos de 2 caracteres
+            novo_caminho = arquivo.with_name(novo_nome)
+            try:
+                arquivo.rename(novo_caminho)
+            except Exception as error:
+                print(f"Ocorreu um erro: {str(error)}")
+                print("Não foi possivel numerar o arquivo: " + arquivo.name) 
+                continue   
+            num += 1
+        pass
+    
+    #função para fechar o aplicativo
+    def exit_app(self):
+        sys.exit()
         
     def build(self):
-        return layout_principal() # Retornando a boxlayout
+        self.title = "RAQIS - Renomeador de Arquivos - Developer by JonesGP Studio"
+
+        return layoutprincipal() # Retornando a boxlayout
 if __name__ == '__main__': #__main__ é uma variavel especial que é definida como __main__ quando voce executa o arquivo diretamente
     Meu_app().run()
     
